b0e3e90857690a16766417816c8b52881ee9e333
Zil Norvilis <zilasino27@gmail.com>
2021-07-19 10:45:13 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ['left-bicep', 'right-bicep', 'waist', 
+        'hips', 'left-thigh', 'right-thigh']
+
+iter = 0
+30.times do |i|
+  iter = 0 if iter >= 5
+  measurement = Measurement.create!(name: names[iter])
+  measure = Measure.create!(data: rand(11.2...76.9), measurement: measurement)
+  iter += 1
+  puts "#{i} created!"
+end
