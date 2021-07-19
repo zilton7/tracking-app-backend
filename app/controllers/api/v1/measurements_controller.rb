554541ee9dc6127377e9b25681dcb0b59f3edd0f
Zil Norvilis <zilasino27@gmail.com>
2021-07-19 14:08:36 +0200

    Add measurement create action

--- app/controllers/api/v1/measurements_controller.rb
+++ app/controllers/api/v1/measurements_controller.rb
@@ -9,10 +9,33 @@ module Api
       end
 
       def show
-        @measurement = Measurement.find(params[:id])
+        @measurement = Measurement.find(measurement_params)
         @measures = Measure.where(measurement: @measurement)
 
         render json: @measurement.to_json(include: {:measures => {only: [:data, :created_at]}})
+      end
+
+      def create
+        p "measurement_params"
+        p measurement_params
+        measurement = Measurement.find(measurement_params['id'])
+        measure = Measure.new(measure_params.merge(measurement_id: measurement.id))
+        
+        if measure.save
+          render json: measure.as_json, status: :created
+        else
+          render json: measure.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def measurement_params
+        params.require(:measurement).permit(:id)
+      end
+
+      def measure_params
+        params.require(:measure).permit(:data)
       end
 
     end
