af9ea524cea2e52fed2127495cf20aa2c866ff30
Zil Norvilis <zilasino27@gmail.com>
2021-07-25 10:35:46 +0200

    Fix linter offences

--- spec/controllers/measurements_controller_spec.rb
+++ spec/controllers/measurements_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 require 'json'
 
 RSpec.describe Api::V1::MeasurementsController, type: :controller do
-  describe "GET index" do
-    it "should receive JSON data" do
+  describe 'GET index' do
+    it 'should receive JSON data' do
       get :index
 
       expect(response).to have_http_status(:success)
--- spec/requests/measurements_spec.rb
+++ spec/requests/measurements_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Measurements", type: :request do
-  describe "GET /measurements" do
-    it "should return all measurements" do
+RSpec.describe 'Measurements', type: :request do
+  describe 'GET /measurements' do
+    it 'should return all measurements' do
       get '/api/v1/measurements'
 
       expect(response).to have_http_status(:success)
@@ -10,28 +10,30 @@ RSpec.describe "Measurements", type: :request do
     end
   end
 
-  describe "POST /measurements" do
-    it "should create a new measurement" do
-      Measurement.create!(name:"test", image:'http://placehold.it/500x500') if Measurement.all.count < 1
-      expect { post '/api/v1/measurements', params: {
-        measurement: {
-          id: Measurement.last.id
-        },
-        measure: {
-          data: 69.96
+  describe 'POST /measurements' do
+    it 'should create a new measurement' do
+      Measurement.create!(name: 'test', image: 'http://placehold.it/500x500') if Measurement.all.count < 1
+      expect do
+        post '/api/v1/measurements', params: {
+          measurement: {
+            id: Measurement.last.id
+          },
+          measure: {
+            data: 69.96
+          }
         }
-      }
-    }.to change {Measure.count}.by(1)
+      end.to change { Measure.count }.by(1)
     end
   end
 
-  describe "GET /measurements/:id" do
+  describe 'GET /measurements/:id' do
     measurement = Measurement.last
-    it "should return single measurement data" do
-      get '/api/v1/measurements/' + measurement.id.to_s
+    it 'should return single measurement data' do
+      get "/api/v1/measurements/#{measurement.id}"
 
       expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body)).to include("name"=>"Test", "image"=>"http://placehold.it/500x500", "measures"=>[])
+      expect(JSON.parse(response.body)).to include('name' => 'Test', 'image' => 'http://placehold.it/500x500',
+                                                   'measures' => [])
     end
   end
 end
